952939f2b46c2d743a305073068190780e573629
Sly <sylvain.giraud78@gmail.com>
2025-12-20 02:12:33 +0100

    repair default_simple.tres and delete old QuestResolutionProfile

--- src/quest_system/quest/QuestResolutionProfile.gd
+++ src/quest_system/quest/QuestResolutionProfile.gd
@@ -11,3 +11,18 @@ extends Resource
 @export var failure_loyal_effects: Array[QuestEffect] = []
 @export var failure_neutral_effects: Array[QuestEffect] = []
 @export var failure_traitor_effects: Array[QuestEffect] = []
+
+func get_effects(choice: String) -> Array[QuestEffect]:
+    match choice:
+        "LOYAL": return success_loyal_effects
+        "NEUTRAL": return success_neutral_effects
+        "TRAITOR": return success_traitor_effects
+        _: return []
+
+
+func get_failure_effects(choice: String) -> Array[QuestEffect]:
+    match choice:
+        "LOYAL": return failure_loyal_effects
+        "NEUTRAL": return failure_neutral_effects
+        "TRAITOR": return failure_traitor_effects
+        _: return []
